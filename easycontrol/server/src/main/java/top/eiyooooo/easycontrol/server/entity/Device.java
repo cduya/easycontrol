7b737f67b9124511acfae9affe7252333adac020
cduya <iduya@foxmail.com>
2025-09-20 09:19:09 +0800

    Update Device.java

--- easycontrol/server/src/main/java/top/eiyooooo/easycontrol/server/entity/Device.java
+++ easycontrol/server/src/main/java/top/eiyooooo/easycontrol/server/entity/Device.java
@@ -200,8 +200,6 @@ public final class Device {
                     IBinder token = SurfaceControl.getPhysicalDisplayToken(physicalDisplayId);
                     if (token != null) SurfaceControl.setDisplayPowerMode(token, mode);
                 }
-                if (mode == Display.STATE_UNKNOWN) ButtonMonitor.start();
-                else ButtonMonitor.stop();
                 return;
             } catch (Exception ignored) {
                 L.w("change power mode for all screens error, try built-in display");
@@ -210,8 +208,6 @@ public final class Device {
         try {
             IBinder d = SurfaceControl.getBuiltInDisplay();
             if (d != null) SurfaceControl.setDisplayPowerMode(d, mode);
-            if (mode == Display.STATE_UNKNOWN) ButtonMonitor.start();
-            else ButtonMonitor.stop();
         } catch (Exception e) {
             L.e("change screen power mode error", e);
         }
